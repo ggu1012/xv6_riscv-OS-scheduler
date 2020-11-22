fd1144cbee29513db12e68ac063fe132041e5215
ggu1012 <70372861+ggu1012@users.noreply.github.com>
2020-11-22 16:29:50 +0900

    private

--- tar.sh
+++ tar.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-#########################################################
-# EEE3535 Operating Systems                             #
-# Written by William J. Song                            #
-# School of Electrical Engineering, Yonsei University   #
-#########################################################
-
 sid=$(grep -w sid user/sid.h | cut -d' ' -f3)
 curdir=$(pwd | awk -F / '{print $NF}')
 make clean > /dev/null;
